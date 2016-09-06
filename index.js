83a2c54786f0079a0a3b79627e34babb45e244c7
harshvit <hmharshith@gmail.com>
2016-09-06 21:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var wagner = require('wagner-core');
 
  require('./models')(wagner);
 var app = express();
-wagner.invoke(require('./auth'), { app: app });
+// wagner.invoke(require('./auth'), { app: app });
 
 app.use('/selectdine_api/v1', require('./api_restaurants')(wagner));
 app.use('/selectdine_api/v1', require('./api_categories')(wagner));
 app.use('/selectdine_api/v1', require('./api_users')(wagner));
 
  app.listen(80);
- console.log('Listening on port 3000!');
+ console.log('Listening on port 80!');
